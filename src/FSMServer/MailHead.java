42716e393f3debe4eba660f221d34e4f2376c38f
ProFatXuanAll <ProFatXuanAll@gmail.com>
2018-06-16 13:38:04 +0800

    add try catch to mail api

--- src/FSMServer/MailHead.java
+++ src/FSMServer/MailHead.java
@@ -1,6 +1,9 @@
 package FSMServer;
 
+import java.util.Date;
+
 public class MailHead extends Head {	
+	private Date timeStemp;
 	public MailHead( String id, String from, String to, String title ) {
 		super( id, from, to, title );
 	}
